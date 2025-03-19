4d903fab831c2655d61eb903b489ec48d3ead7e6
SverreNystad <sverrenystad@gmail.com>
2025-03-19 21:04:43 +0100

    refactor: make summarize function typehint return of AIMessage in summarizer.py

--- src/summarizer.py
+++ src/summarizer.py
@@ -1,8 +1,11 @@
 from langchain_ollama import ChatOllama
+from langchain_core.messages import AIMessage
 
 llm = ChatOllama(model="mistral")
 
 
-def summarize(text: str):
+def summarize(text: str) -> AIMessage:
     prompt = f"Summarize the following meeting:\n\n{text}"
     return llm.invoke(prompt)
+
+
